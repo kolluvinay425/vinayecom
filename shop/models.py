453c11c5afad8c5371ec04adcaa9b93aa1d9bc89
vinay kollu <kolluvinay425@gmail.com>
2020-07-15 13:44:59 +0530

    first commit

---
+++ shop/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+
+    def __str__(self):
+        return self.title
+    title = models.CharField(max_length=200)
+    price = models.FloatField()
+    discount_price = models.FloatField()
+    category = models.CharField(max_length=200)
+    discription = models.TextField()
+    image = models.CharField(max_length=300)
+
+class Order(models.Model):
+
+    def __str__(self):
+        return self.city
+    items = models.CharField(max_length=500)
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    address =models.CharField(max_length=1000)
+    city =models.CharField(max_length=200)
+    state =models.CharField(max_length=200)
+    zipcode =models.CharField(max_length=200)
+    total = models.CharField(max_length=200)    
